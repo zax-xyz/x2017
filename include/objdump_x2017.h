59d28ed509b187451a1905dc1ae376e940902b78
Michael Vo <zax@zaxu.xyz>
2021-04-13 23:52:24 +1000

    comments

--- include/objdump_x2017.h
+++ include/objdump_x2017.h
@@ -6,7 +6,15 @@
 
 #include <err.h>
 
+/*
+ * The bulk of the program. Outputs the x2017 assembly instructions in a human-
+ * readable format
+ */
 void objdump(const func_t* functions);
+
+/*
+ * Prints a single argument from an x2017 instruction in a human-readable format
+ */
 void print_arg(const arg_t arg, const char** field_types);
 
 #endif
